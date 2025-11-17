6ac14865887157e633363badada47be7cdcccedc
ILYA <106765406+D15ND@users.noreply.github.com>
2025-11-17 22:31:27 +0300

    feat: add solution

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -134,8 +134,55 @@ function convertToRomanNumerals(/* num */) {
  *  '10,5'    => 'one zero point five'
  *  '1950.2'  => 'one nine five zero point two'
  */
-function convertNumberToString(/* numberStr */) {
-  throw new Error('Not implemented');
+function convertNumberToString(numberStr) {
+  let result = '';
+  for (let i = 0; i < numberStr.length; i += 1) {
+    const num = numberStr[i];
+    if (num === '-') {
+      if (result !== '') result += ' ';
+      result += 'minus';
+    } else if (num === '.' || num === ',') {
+      if (result !== '') result += ' ';
+      result += 'point';
+    } else {
+      if (result !== '') result += ' ';
+      switch (num) {
+        case '0':
+          result += 'zero';
+          break;
+        case '1':
+          result += 'one';
+          break;
+        case '2':
+          result += 'two';
+          break;
+        case '3':
+          result += 'three';
+          break;
+        case '4':
+          result += 'four';
+          break;
+        case '5':
+          result += 'five';
+          break;
+        case '6':
+          result += 'six';
+          break;
+        case '7':
+          result += 'seven';
+          break;
+        case '8':
+          result += 'eight';
+          break;
+        case '9':
+          result += 'nine';
+          break;
+        default:
+          break;
+      }
+    }
+  }
+  return result;
 }
 
 /**
